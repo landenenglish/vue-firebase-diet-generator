53bc3232488473fd1576a515c414b7271b6daa2f
landenenglish <englishlanden@gmail.com>
2022-06-29 19:53:23 -0400

    using different method for protecting apikey

--- src/firebase.js
+++ src/firebase.js
@@ -2,15 +2,7 @@ import firebase from 'firebase/compat/app'
 import 'firebase/compat/auth'
 import 'firebase/compat/firestore'
 import { ref, onUnmounted } from 'vue'
-
-const config = {
-  apiKey: process.env.VUE_APP_FIREBASE_APIKEY,
-  authDomain: process.env.VUE_APP_FIREBASE_AUTHDOMAIN,
-  projectId: process.env.VUE_APP_FIREBASE_PROJECTID,
-  storageBucket: process.env.VUE_APP_FIREBASE_STORAGEBUCKET,
-  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGINGSENDERID,
-  appId: process.env.VUE_APP_FIREBASE_APPID,
-}
+import config from '@/apikey'
 
 const firebaseApp = firebase.initializeApp(config)
 
