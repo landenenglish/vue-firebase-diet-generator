9a70a089a237842c2d6ee0cdd81a9d6a31165509
landenenglish <englishlanden@gmail.com>
2022-07-07 13:50:07 -0400

    styling

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Menu</a>
+      <a class="navbar-brand" href="#"></a>
       <button
         class="navbar-toggler"
         type="button"
